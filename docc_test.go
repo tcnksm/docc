2600fe1a8ede087b1a840f75afa335852dd2d105
zoncoen <zoncoen@gmail.com>
2014-06-13 03:47:13 +0900

    Make more readable

--- docc_test.go
+++ docc_test.go
@@ -5,66 +5,27 @@ import (
 )
 
 func TestConvertToHTTPS(t *testing.T) {
+	args := []string{
+		"github.com:tcnksm/docc.git",
+		"git@github.com:tcnksm/docc.git",
+		"github.com:/~user/tcnksm/docc.git",
+		"ssh://github.com/tcnksm/docc.git",
+		"ssh://git@github.com/tcnksm/docc.git",
+		"ssh://github.com:9418/tcnksm/docc.git",
+		"ssh://github.com/~user/tcnksm/docc.git",
+		"git://github.com/tcnksm/docc.git",
+		"git://github.com:9418/tcnksm/docc.git",
+		"git://github.com/~user/tcnksm/docc.git",
+		"https://github.com/tcnksm/docc.git",
+		"https://github.com:9418/tcnksm/docc.git",
+	}
+
 	expected := "https://github.com/tcnksm/docc"
-	got := ""
 
-	got = convertURLToHTTPS("github.com:tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("git@github.com:tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("github.com:/~user/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("ssh://github.com/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("ssh://git@github.com/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("ssh://github.com:9418/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("ssh://github.com/~user/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("git://github.com/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("git://github.com:9418/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("git://github.com/~user/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("https://github.com/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
-	}
-
-	got = convertURLToHTTPS("https://github.com:9418/tcnksm/docc.git")
-	if got != expected {
-		t.Errorf("Expected '%v', but got '%v'.", expected, got)
+	for _, arg := range args {
+		got := convertURLToHTTPS(arg)
+		if got != expected {
+			t.Errorf("Expected '%v', but got '%v'.", expected, got)
+		}
 	}
 }
